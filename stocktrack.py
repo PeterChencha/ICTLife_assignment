5d9147a2840e4fcc65b5296bdf9bcad6e04ae724
extraordinaire01 <peter@chenchatech.com>
2020-04-01 23:30:24 +0300

    Scrap yahoo finance first, it worked, now trying google. Also created virtual env.

--- stocktrack.py
+++ stocktrack.py
@@ -1,14 +1,38 @@
+import bs4
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
 class StockTrack(object):
     """Provided a stock symbol find its current price."""
     def __init__(self, symbol):
         super(StockTrack, self).__init__()
         self.symbol = symbol
 
+    def createYahooQuery(self):
+        url = "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
+        try:
+            page = urlopen(url)
+            soup = bs4.BeautifulSoup(page,'html.parser')
+            price = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+            return price
+        except:
+            print("Error Opening the Url")
+
     def createQuery(self):
-        pass
+        url = "https://google.com/search?q={self.symbol}"
+        try:
+            page = urlopen(url)
+            soup = bs4.BeautifulSoup(page,'html.parser')
+            return soup
+        except:
+            print("Error Opening the Url")
 
     def processQuery(self):
-        pass
+        unprocessed_info = createQuery()
+        price = unprocessed_info.find('div',{'id': 'center_col'}).find('span').text
+
+
 
     def getPrice(self):
         try:
@@ -21,7 +45,12 @@ class StockTrack(object):
             raise e
 
 
-input = raw_input ("Enter stock symbol :")
+# input = raw_input ("Enter stock symbol :")
+# stockprice = StockTrack(input)
+# results = stockprice.getPrice()
+# print (results)
+
+input = "aapl"
 stockprice = StockTrack(input)
-results = stockprice.getPrice()
+results = stockprice.createYahooQuery()
 print (results)
