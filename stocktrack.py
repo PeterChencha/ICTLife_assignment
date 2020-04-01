68f80f89084cda88bb38560df1678332efe9a1a4
extraordinaire01 <peter@chenchatech.com>
2020-04-01 15:57:55 +0300

    Setup functions that will be used.

--- stocktrack.py
+++ stocktrack.py
@@ -1,15 +1,22 @@
 class StockTrack(object):
-    """docstring for StockTrack."""
+    """Provided a stock symbol find its current price."""
     def __init__(self, symbol):
         super(StockTrack, self).__init__()
         self.symbol = symbol
+
+    def createQuery(self):
+        pass
+
+    def processQuery(self):
+        pass
 
     def getPrice(self):
         try:
             if self.symbol == "aapl":
                 return 254.29
             else:
-                error = " Symbol not found"
+                error = "Symbol not found"
+                return error
         except Exception as e:
             raise e
 
