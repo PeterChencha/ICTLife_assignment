0fb5c028b9b87a1f99e38f1061560d0a25ffa4cf
extraordinaire01 <peter@chenchatech.com>
2020-04-02 07:25:18 +0300

    Add unit to results.

--- stocktrack.py
+++ stocktrack.py
@@ -8,9 +8,10 @@ class StockTrack(object):
     def __init__(self, symbol):
         super(StockTrack, self).__init__()
         self.symbol = symbol
+        self.stockprice = {}
 
     def createYahooQuery(self):
-        url = "https://finance.yahoo.com/quote/{}?p=AAPL&.tsrc=fin-srch".format(self.symbol)
+        url = "https://finance.yahoo.com/quote/{}?p={}&.tsrc=fin-srch".format(self.symbol, self.symbol)
         #print (url)
         try:
             page = urlopen(url)
@@ -23,7 +24,9 @@ class StockTrack(object):
         unprocessed_info = self.createYahooQuery()
         try:
             price = unprocessed_info.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
-            return price
+            self.stockprice['name'] = self.symbol
+            self.stockprice['current_price'] = '{}USD'.format(price)
+            return self.stockprice
         except:
             error = "Not the div we want"
             return error
@@ -42,28 +45,35 @@ class StockTrack(object):
 
     def processGoogleQuery(self):
         unprocessed_info = self.createGoogleQuery()
+        sections = unprocessed_info.find_all('g')
+        #print (sections)
+        return sections
         #price = unprocessed_info.find('div',{'id': 'center_col'}).find('span').text
         #print (unprocessed_info)
-        try:
-            price = unprocessed_info.find('div',{'class': 'OiIFo'}).find('span').text
-            return price
-        except:
-            error = "Not the div we want"
-            return error
+
+        # try:
+        #     self.price['name'] = sections[0].div.text
+        #     spans = sections[1].find_all('div', recursive=False)[1].find_all('span', recursive=False)
+        #     self.price['current_price'] = spans[0].text
+        #     self.price['current_change'] = spans[1].text
+        #     return self.price
+        # except:
+        #     error = "Not the div we want"
+        #     return error
 
 
 
 
 #GOOGLE IMPLEMENTATION
-input = input ("Enter stock symbol :")
-stockprice = StockTrack(input)
-results = stockprice.processGoogleQuery()
-print (results)
+# input = input ("Enter stock symbol :")
+# stockprice = StockTrack(input)
+# results = stockprice.processGoogleQuery()
+# print (results)
 
 
 
 #YAHOO IMPLEMENTATION
-# input = input ("Enter stock symbol :")
-# stockprice = StockTrack(input)
-# results = stockprice.processYahooQuery()
-# print (results)
+input = input ("Enter stock symbol :")
+stockprice = StockTrack(input)
+results = stockprice.processYahooQuery()
+print (results)
